78e2cf753f6bdb79ccf052d2409c33fc25472b41
elishakay <kramer1346@gmail.com>
2018-03-20 14:53:41 +0200

    first commit

--- Client/jstests/lib/jasmine-standalone/spec/SpecHelper.js
+++
@@ -1,8 +0,0 @@
-beforeEach(function() {
-  this.addMatchers({
-    toHaveSameId: function(expectedItem) {
-      var item = this.actual;
-      return item && expectedItem && expectedItem && item.id === expectedItem.id;
-    }
-  });
-});
